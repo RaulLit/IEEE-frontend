ee04fb73c006fba66e2634eec542c47265fa75cd
Aditya Raul <raul.aditya912@gmail.com>
2023-03-24 12:29:52 +0530

    Updated section 3 of home page & added glitch animations

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,14 +6,14 @@ const Home = () => {
         <div className="home-container">
             <div className="landing-section">
                 <div className="landing-text">
-                    <h2 className="glitch">Glitch</h2>
+                    <h2 className="glitch"><span>Glitch</span></h2>
                 </div>
             </div>
 
             <div className="section section1">
                 <div className="img"></div>
                 <div className="text">
-                    <h1>Did you heard what happened?</h1>
+                    <h1>Did you heard what <span>happened?</span></h1>
                     <p>As Sarah walked down the street, she suddenly felt a strange sensation wash over her. It was like she had been in this exact moment before, but she knew she hadn't. She shrugged it off and continued walking until she saw a man walking towards her. He looked familiar, but she couldn't place where she had seen him before. As they passed each other, she heard him mutter, "glitch in the matrix." Sarah stopped in her tracks and turned around, but the man had already disappeared. Confused and a bit frightened, she wondered what it all meant and if her reality was really just a simulation after all.</p>
                 </div>
             </div>
@@ -30,8 +30,9 @@ const Home = () => {
             <div className="section section3">
                 <div className="img"></div>
                 <div className="text">
-                    <h1>Catchy title</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt eaque illum qui tempora veritatis sed sit exercitationem suscipit hic. Eaque molestias maxime quis ullam reprehenderit aspernatur fugiat architecto cumque qui ipsa eius iusto iure dolor, obcaecati consequatur omnis dolores corrupti illo! Similique quasi nisi magni, repudiandae iusto velit. Officiis, impedit!</p>
+                    <h1>Compete against the best</h1>
+                    <p>Are you ready to experience a glitch in the matrix? Step into a world of thrilling adventure and captivating mystery - <strong>register now</strong> to unlock the secrets that await you at our unforgettable event!</p>
+                <NavLink to="register"><button>Register now</button></NavLink>
                 </div>
             </div>
         </div>
